41678d555ace46a7a5a839f8d7f9ea6dedaad112
kt-postnikova <kt-postnikova@yandex.ru>
2021-09-20 12:18:54 +0300

    Создан компонент App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,30 +2,36 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import '../../index.css';
 import Header from '../Header/Header';
-import Navigation from '../Navigation/Navigation';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
 import SavedMovies from '../SavedMovies/SavedMovies';
 import Profile from '../Profile/Profile';
 import Login from '../Login/Login';
 import Register from '../Register/Register';
-import Movies from '../Movies/Movies'
+import Movies from '../Movies/Movies';
+import PageNotFound from '../PageNotFound/PageNotFound';
 
 function App() {
   return (
     <>
-      <Header></Header>
       <Switch>
         <Route exact path="/">
+          <Header></Header>
           <Main></Main>
+          <Footer></Footer>
         </Route>
         <Route path="/movies">
+          <Header></Header>
           <Movies></Movies>
+          <Footer></Footer>
         </Route>
         <Route path="/saved-movies">
+          <Header></Header>
           <SavedMovies></SavedMovies>
+          <Footer></Footer>
         </Route>
         <Route path="/profile">
+          <Header></Header>
           <Profile></Profile>
         </Route>
         <Route path="/signin">
@@ -34,8 +40,11 @@ function App() {
         <Route path="/signup">
           <Register></Register>
         </Route>
+        <Route path="*">
+          <PageNotFound />
+        </Route>
       </Switch>
-      <Footer></Footer>
+      {/* <Footer></Footer> */}
     </>
   );
 }
