ee7eb641b7449f5d3e19ec0222f0f21f8d213f27
kt-postnikova <kt-postnikova@yandex.ru>
2021-09-20 12:16:37 +0300

    Создан компонент Preloader

---
+++ src/components/Preloader/Preloader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Preloader(params) {
+    return (
+        <div className="preloader container">
+            <button className="preloader__button">Ещё</button>
+        </div>
+    )
+}
+
+export default Preloader;
