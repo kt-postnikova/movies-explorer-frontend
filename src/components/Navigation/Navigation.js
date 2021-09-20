36d9eda44bd3737763a7a7915bf9adb8b783885b
kt-postnikova <kt-postnikova@yandex.ru>
2021-09-20 12:18:16 +0300

    Создан компонент Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import accountImage from '../../images/account.svg';
 
 function Navigation(params) {
+    const userLocation = useLocation();
+
     return (
         <nav className="navigation">
             <div className="nav-links">
-                <Link to="/movies" className='nav-links__link nav-links__link_active'>Фильмы</Link>
-                <Link to="/saved-movies" className="nav-links__link">Сохраненные фильмы</Link>
+                <Link to="/movies" className={`nav-links__link  ${userLocation.pathname === '/movies' ? ('nav-links__link_active') : ''}`}>Фильмы</Link>
+                <Link to="/saved-movies" className={`nav-links__link  ${userLocation.pathname === '/saved-movies' ? ('nav-links__link_active') : ''}`}>Сохраненные фильмы</Link>
             </div>
             <div className="account-link">
                 <p className="account-link__name">Аккаунт</p>
@@ -17,7 +19,7 @@ function Navigation(params) {
                     </div>
                 </Link>
             </div>
-        </nav>
+        </nav >
     )
 }
 
