e200cb34c206884e72249cfe1586cf50d3ec99aa
kt-postnikova <kt-postnikova@yandex.ru>
2021-09-27 16:59:37 +0300

    Адаптирован блок Profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Profile(params) {
     return (
@@ -16,7 +17,9 @@ function Profile(params) {
                 </label>
                 <button className="profile__button">Редактировать</button>
             </form>
-            <button className="signout-button">Выйти из аккаунта</button>
+            <Link className="signout-button" to="/signin">
+                <button className="signout-button">Выйти из аккаунта</button>
+            </Link>
         </div>
     )
 }
