f6ae2dcf04642f0a63e12eef67ccb764f4823670
kt-postnikova <kt-postnikova@yandex.ru>
2021-09-27 17:00:50 +0300

    Адаптирован блок Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import accountImage from '../../images/account.svg';
+import BurgerMenu from '../BurgerMenu/BurgerMenu';
+import AccountButton from '../AccountButton/AccountButton';
 
 function Navigation(params) {
     const userLocation = useLocation();
@@ -11,14 +12,8 @@ function Navigation(params) {
                 <Link to="/movies" className={`nav-links__link  ${userLocation.pathname === '/movies' ? ('nav-links__link_active') : ''}`}>Фильмы</Link>
                 <Link to="/saved-movies" className={`nav-links__link  ${userLocation.pathname === '/saved-movies' ? ('nav-links__link_active') : ''}`}>Сохраненные фильмы</Link>
             </div>
-            <div className="account-link">
-                <p className="account-link__name">Аккаунт</p>
-                <Link to="/profile" className="account-link__button">
-                    <div className="account-link__button-block">
-                        <img className="account-link__button-image" src={accountImage} alt="" />
-                    </div>
-                </Link>
-            </div>
+            <BurgerMenu></BurgerMenu>
+            <AccountButton></AccountButton>
         </nav >
     )
 }
