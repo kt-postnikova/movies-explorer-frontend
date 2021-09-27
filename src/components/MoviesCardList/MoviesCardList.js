b61ae1b12906c8b4777b440c505745082eb1bdeb
kt-postnikova <kt-postnikova@yandex.ru>
2021-09-27 17:01:10 +0300

    Адаптирован блок MoviesCardList

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import MoviesCard from '../MoviesCard/MoviesCard';
+// import Preloader from '../Preloader/Preloader';
 
 function MoviesCardList(params) {
     return (
-        <section className="movies-cards container">
-            <MoviesCard></MoviesCard>
-            <MoviesCard></MoviesCard>
-            <MoviesCard></MoviesCard>
-            <MoviesCard></MoviesCard>
-            <MoviesCard></MoviesCard>
-            <MoviesCard></MoviesCard>
-            <MoviesCard></MoviesCard>
-            <MoviesCard></MoviesCard>
-        </section>
+        <>
+            <section className="movies-cards container">
+                {/* <Preloader></Preloader> */}
+                <div className="movies-cards__list">
+                    <MoviesCard></MoviesCard>
+                    <MoviesCard></MoviesCard>
+                    <MoviesCard></MoviesCard>
+                    <MoviesCard></MoviesCard>
+                    <MoviesCard></MoviesCard>
+                    <MoviesCard></MoviesCard>
+                    <MoviesCard></MoviesCard>
+                    <MoviesCard></MoviesCard>
+                    <MoviesCard></MoviesCard>
+                    <MoviesCard></MoviesCard>
+                    <MoviesCard></MoviesCard>
+                    <MoviesCard></MoviesCard>
+                </div>
+            </section>
+            <div className="movies-cards__button-container">
+                <button className="movies-cards__button">Ещё</button>
+            </div>
+        </>
     )
 }
 
