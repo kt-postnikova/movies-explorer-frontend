c81d6b933280d743458af3c1c9d8dde3d6a731f8
kt-postnikova <kt-postnikova@yandex.ru>
2021-09-27 17:02:34 +0300

    Добавлен alt

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -4,7 +4,7 @@ import cardImage from '../../images/pic1.png';
 function MoviesCard(params) {
     return (
         <div className="card">
-            <img className="card__image" src={cardImage} alt="" />
+            <img className="card__image" src={cardImage} alt="Постер фильма" />
             <div className="card__info">
                 <p className="card__name">33 слова о дизайне</p>
                 <div className="card__select"></div>
