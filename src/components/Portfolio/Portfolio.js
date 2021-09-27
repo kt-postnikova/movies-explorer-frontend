815830877fc9181275eeea82bb0e070e6a7ce0b0
kt-postnikova <kt-postnikova@yandex.ru>
2021-09-27 17:00:25 +0300

    Адаптирован блок Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import linkImg from '../../images/link.svg';
 
 function Portfolio(params) {
@@ -8,24 +7,22 @@ function Portfolio(params) {
             <h2 className="portfoilio__header">Портфолио</h2>
             <div className="portfolio__links">
                 <div className="portfolio__link">
-                    <Link className="portfolio__name">Статичный сайт
-                        <img className="portfolio__link-img" src={linkImg} alt="" />
+                    <a className="portfolio__name" href="https://github.com/kt-postnikova/how-to-learn" target="_blank" rel="noreferrer">Статичный сайт
+                        <img className="portfolio__link-img" src={linkImg} alt="Иконка стрелки" />
                         <div className="line"></div>
-                    </Link>
-
+                    </a>
                 </div>
                 <div className="portfolio__link">
-                    <Link className="portfolio__name">Адаптивный сайт
-                        <img className="portfolio__link-img" src={linkImg} alt="" />
+                    <a className="portfolio__name" href="https://github.com/kt-postnikova/russian-travel" target="_blank" rel="noreferrer">Адаптивный сайт
+                        <img className="portfolio__link-img" src={linkImg} alt="Иконка стрелки" />
                         <div className="line"></div>
-                    </Link>
-
+                    </a>
                 </div>
                 <div className="portfolio__link">
-                    <Link className="portfolio__name">Одностраничное приложение
-                        <img className="portfolio__link-img" src={linkImg} alt="" />
-                    </Link>
-
+                    <a className="portfolio__name" href="https://github.com/kt-postnikova/react-mesto-api-full" target="_blank" rel="noreferrer">Одностраничное приложение
+                        <img className="portfolio__link-img" src={linkImg} alt="Иконка стрелки" />
+                        <div className="line"></div>
+                    </a>
                 </div>
             </div>
         </section>
