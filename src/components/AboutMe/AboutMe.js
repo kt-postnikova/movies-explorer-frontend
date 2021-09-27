0f37a51af7ff247c2e0e0ee29c18e8fe0f4d22d9
kt-postnikova <kt-postnikova@yandex.ru>
2021-09-27 16:58:27 +0300

    Адаптирован блок AboutMe

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import studentImage from '../../images/student-pic.png';
 
 function AboutMe(params) {
@@ -8,14 +7,14 @@ function AboutMe(params) {
             <h2 className="title">Студент</h2>
             <div className="header-line"></div>
             <div className="about-me__info">
+                <img className="about-me__image" src={studentImage} alt="Фото студента" />
                 <h2 className="about-me__header">Виталий</h2>
                 <p className="about-me__subheader">Фронтенд-разработчик, 30 лет</p>
                 <p className="about-me__bio">Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
                     и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
-                <img className="about-me__image" src={studentImage} alt="" />
                 <div className="social">
-                    <Link className="social__link">Facebook</Link>
-                    <Link className="social__link">Github</Link>
+                    <a className="social__link" href="https://vk.com/saintcath" target="_blank" rel="noreferrer">VK</a>
+                    <a className="social__link" href="https://github.com/kt-postnikova" target="_blank" rel="noreferrer">Github</a>
                 </div>
             </div>
         </section>
