4b73b2a1bb7a7a3f842141051821345b586849d2
kt-postnikova <kt-postnikova@yandex.ru>
2021-10-05 13:17:26 +0300

    Иконка стрелки теперь кнопка поиска

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -8,7 +8,7 @@ function SearchForm(params) {
             <div className="search-form__container">
                 <img className="search-form__search-image" src={searchImage} alt="Иконка лупа" />
                 <input className="search-form__input" placeholder="Фильм" type="text" required />
-                <img className="search-form__find-image" src={findImage} alt="Иконка стрелка" />
+                <button className="search-form__find-image"><img src={findImage} alt="Иконка стрелка" /></button>
             </div>
             <div className="line"></div>
         </section>
