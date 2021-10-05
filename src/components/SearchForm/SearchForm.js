39580a321a53858c5ca41abe2f11c931ebbd7f7f
kt-postnikova <kt-postnikova@yandex.ru>
2021-10-05 17:52:55 +0300

    Добавлен сабмит кнопки поиска

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,9 +3,14 @@ import searchImage from '../../images/search.svg';
 import findImage from '../../images/find.svg';
 
 function SearchForm(params) {
+
+    function handleSubmit(event) {
+        event.preventDefault()
+    }
+
     return (
         <section className="search-form container">
-            <form className="search-form__container" noValidate>
+            <form className="search-form__container" onSubmit={handleSubmit} noValidate>
                 <img className="search-form__search-image" src={searchImage} alt="Иконка лупа" />
                 <input className="search-form__input" placeholder="Фильм" type="text" required />
                 <button className="search-form__find-image"><img src={findImage} alt="Иконка стрелка" /></button>
