09dd4dc0a807acd155af5a823d887e71f561e295
kt-postnikova <kt-postnikova@yandex.ru>
2021-10-14 11:05:40 +0300

    Созданы функции запросов к API

---
+++ src/utils/MainApi.js
@@ -0,0 +1,78 @@
+const getResponse = response => response.ok ? response.json() : Promise.reject(`Ошибка ${response.status}`)
+
+const LOCAL_URL = 'http://localhost:3000';
+const PROD_URL = 'https://api.movie-explorer.nomoredomains.club/signup';
+
+export const register = (email, password, name) => {
+    return fetch(`${LOCAL_URL}/signup`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password, name })
+    })
+        .then(getResponse)
+};
+
+export const authorize = (email, password) => {
+    return fetch(`${LOCAL_URL}/signin`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+    })
+        .then(getResponse)
+}
+
+export const getContent = () => {
+    return fetch(`${LOCAL_URL}/users/me`, {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `${JSON.parse(localStorage.getItem('token'))}`
+        }
+    })
+        .then(getResponse)
+}
+
+export const editUserInfo = (userInfo) => {
+    return fetch(`${LOCAL_URL}/users/me`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            "Authorization": `${JSON.parse(localStorage.getItem('token'))}`
+        },
+        body: JSON.stringify({
+            email: userInfo.email,
+            name: userInfo.name,
+        })
+    })
+        .then(getResponse)
+}
+
+export const selectMovie = (movie) => {
+    return fetch(`${LOCAL_URL}/movies`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `${JSON.parse(localStorage.getItem('token'))}`
+        },
+        body: JSON.stringify({
+            country: movie.country,
+            director: movie.director,
+            duration: movie.duration,
+            year: movie.year,
+            description: movie.description,
+            image: `${'https://api.nomoreparties.co' + movie.image.url}`,
+            trailerLink: movie.trailerLink,
+            thumbnail: `${'https://api.nomoreparties.co' + movie.image.formats.thumbnail.url}`,
+            nameRU: movie.nameRU,
+            nameEN: movie.nameEN,
+            movieId: movie.id,
+        })
+    })
+        .then(getResponse)
+}
