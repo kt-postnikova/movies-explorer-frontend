7d8b73bd8bf88344a9c60cbdde553959e0da6f93
kt-postnikova <kt-postnikova@yandex.ru>
2021-10-14 11:08:17 +0300

    Создана страница регистрации

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,28 +1,52 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useEffect } from 'react/cjs/react.development';
 import logo from '../../images/logo.svg';
+import Form from '../Form/Form';
 
-function Register(params) {
+function Register({ onRegister, message }) {
+
+    const [values, setValues] = React.useState({
+        name: '',
+        email: '',
+        password: ''
+    });
+    const [errors, setErrors] = React.useState({});
+    const [isValid, setIsValid] = React.useState(false);
+
+    const handleChange = (event) => {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        setValues({ ...values, [name]: value });
+        setErrors({ ...errors, [name]: target.validationMessage });
+        setIsValid(target.closest("form").checkValidity());
+    };
+
+    function handleRegisterSubmit(e) {
+        e.preventDefault();
+
+        onRegister(values.email, values.password, values.name)
+    }
+
     return (
         <div className="register">
             <Link to="/signup"><img src={logo} alt="Логотип" /></Link>
             <h1 className="register__header">Добро пожаловать!</h1>
-            <form className="form">
+            <Form name="register" button="Зарегистрироваться" onSubmit={handleRegisterSubmit} isFormValid={isValid} message={message} question="Уже зарегистрированы?" link="/signin" linkName="Войти">
                 <label className="form__label">Имя
-                    <input className="form__input" required />
-                    <span className="form__input-error"></span>
+                    <input className="form__input" value={values.name} onChange={handleChange} name="name" required minLength="2" maxLength="30" />
+                    <span className="form__input-error">{errors.name}</span>
                 </label>
                 <label className="form__label">E-mail
-                    <input className="form__input" required />
-                    <span className="form__input-error"></span>
+                    <input className="form__input" type="email" name="email" onChange={handleChange} value={values.email} required />
+                    <span className="form__input-error">{errors.email}</span>
                 </label>
                 <label className="form__label">Пароль
-                    <input className="form__input" required />
-                    <span className="form__input-error">Что-то пошло не так...</span>
+                    <input className="form__input" type="password" name="password" onChange={handleChange} value={values.password} required minLength="8" />
+                    <span className="form__input-error">{errors.password}</span>
                 </label>
-                <button className="form__button">Зарегистрироваться</button>
-                <p className="form__signin">Уже зарегистрированы? <Link to="/signin" className="form__signin-link">Войти</Link></p>
-            </form>
+            </Form>
         </div>
     )
 }
