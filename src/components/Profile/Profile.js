f5ca1b4bc5e6fb9464fdd9bd708e5cbca3517bf1
kt-postnikova <kt-postnikova@yandex.ru>
2021-10-14 11:08:46 +0300

    Создана страница профиля

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,26 +1,62 @@
 import React from 'react';
+import Header from '../Header/Header';
+import { CurrentUserContext } from '../../context/CurrentUserContext';
 import { Link } from 'react-router-dom';
 
-function Profile(params) {
+function Profile({ onEditProfile, loggedIn, onSignOut, message }) {
+
+    const currentUser = React.useContext(CurrentUserContext);
+
+    const [values, setValues] = React.useState({
+        name: currentUser.name,
+        email: currentUser.email
+    });
+    const [errors, setErrors] = React.useState({});
+    const [isValid, setIsValid] = React.useState(false);
+
+    const handleChange = (event) => {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        setValues({ ...values, [name]: value });
+        setErrors({ ...errors, [name]: target.validationMessage });
+        setIsValid(target.closest("form").checkValidity());
+    };
+
+    console.log(errors);
+
+    function handleRegisterSubmit(e) {
+        e.preventDefault();
+
+        onEditProfile(values)
+    }
+
+
     return (
-        <div className="profile container">
-            <h1 className="profile__title">Привет, Виталий!</h1>
-            <form className="profile__form">
-                <label className="profile__label">
-                    <span className="profile__name">Имя</span>
-                    <input className="profile__input" type="text" placeholder="Виталий" />
-                </label>
-                <div className="line"></div>
-                <label className="profile__label">
-                    <span className="profile__name">Email</span>
-                    <input className="profile__input" type="text" placeholder="pochta@yandex.ru" />
-                </label>
-                <button className="profile__button">Редактировать</button>
-            </form>
-            <Link className="signout-button" to="/signin">
-                <button className="signout-button">Выйти из аккаунта</button>
-            </Link>
-        </div>
+        <>
+            <Header loggedIn={loggedIn}></Header>
+            <div className="profile container">
+                <h1 className="profile__title">Привет, {currentUser.name}!</h1>
+                <form className="form" onSubmit={handleRegisterSubmit} >
+                    <label className="profile__label">
+                        <span className="profile__name">Имя</span>
+                        <input className="profile__input" value={values.name} onChange={handleChange} name="name" type="text" placeholder={currentUser.name} minLength="2" maxLength="30" />
+                    </label>
+                    <span className="profile__input-error">{errors.name}</span>
+                    <div className="line"></div>
+                    <label className="profile__label">
+                        <span className="profile__name">Email</span>
+                        <input className="profile__input" value={values.email} onChange={handleChange} name="email" type="email" placeholder={currentUser.email} />
+                    </label>
+                    <span className="profile__input-error">{errors.email}</span>
+                    <p className="profile__message">{message}</p>
+                    <button className="profile__button" disabled={isValid ? null : 'disabled'}>Редактировать</button>
+                </form>
+                <Link className="signout-button" to="/">
+                    <button className="signout-button" onClick={onSignOut}>Выйти из аккаунта</button>
+                </Link>
+            </div >
+        </>
     )
 }
 
