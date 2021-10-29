f7b75775a35bc630d650cb7d6fb9d1330c480ec4
kt-postnikova <kt-postnikova@yandex.ru>
2021-10-29 12:22:39 +0300

    Добавлен поиск по сохраненным фильмам

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,14 +1,40 @@
 import React from 'react';
+import Header from '../Header/Header';
 import SearchForm from '../SearchForm/SearchForm';
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import Footer from '../Footer/Footer';
+import { filterMovies } from '../../utils/filterMovies';
 
-function SavedMovies(params) {
+function SavedMovies({ loggedIn, savedMovies, onDelete }) {
+
+    const [selectedMovies, setSelectedMovies] = React.useState(savedMovies);
+    const [filteredMovies, setFilteredMovies] = React.useState(selectedMovies);
+    const [query, setQuery] = React.useState('');
+
+    /* забираем значение из инпута */
+    function handleQueryChange(e) {
+        setQuery(e.target.value);
+    }
+
+    /* получаем отфильтрованные фильмы по сабмиту */
+    function handleGetMoviesSubmit() {
+        const filteredMovies = filterMovies(savedMovies, query);
+        if (filteredMovies.length === 0) {
+            console.log(filteredMovies);
+        } else {
+            setFilteredMovies(filteredMovies);
+            setSelectedMovies(filteredMovies)
+        }
+    }
+
     return (
         <>
-            <SearchForm></SearchForm>
+            <Header loggedIn={loggedIn}></Header>
+            <SearchForm onSubmit={handleGetMoviesSubmit} onChange={handleQueryChange} query={query}></SearchForm>
             <FilterCheckbox></FilterCheckbox>
-            <MoviesCardList></MoviesCardList>
+            <MoviesCardList savedMovies={savedMovies} moviesList={selectedMovies} onDelete={onDelete}></MoviesCardList>
+            <Footer></Footer>
         </>
     )
 }
