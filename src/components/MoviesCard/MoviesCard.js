a7bfd08fc38ae35e1ac485fae0667ec6a004dcf0
kt-postnikova <kt-postnikova@yandex.ru>
2021-10-29 12:23:44 +0300

    Добавлено сохранение и удаление фильма

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import cardImage from '../../images/pic1.png';
+import { useLocation } from 'react-router';
 
-function MoviesCard(props) {
+function MoviesCard({ movie, savedMovies, onSave, onDelete }) {
+    const userLocation = useLocation();
+    const isSavedMoviesPage = userLocation.pathname === '/saved-movies';
+
+    function handleMovieSave() {
+        onSave(movie);
+    }
+
+    function handleMovieDelete() {
+        onDelete(movie);
+    }
+
+    const cardLikeButtonClassName = (
+        `${savedMovies ? 'card__select card__select_active' : 'card__select'}`
+    );
+
     return (
         <div className="card">
-            <img className="card__image" src={cardImage} alt="Постер фильма" />
+            <img className="card__image" src={(movie.image.url) ? `https://api.nomoreparties.co${movie.image.url}` : movie.image} alt="Постер фильма" />
             <div className="card__info">
-                <p className="card__name">33 слова о дизайне</p>
-                <div className={`card__select ${props.isActive}`}></div>
-                <p className="card__duration">1ч42м</p>
+                <p className="card__name">{movie.nameRU}</p>
+                <button type="button" className={isSavedMoviesPage ? 'card__delete' : cardLikeButtonClassName} onClick={savedMovies ? handleMovieDelete : handleMovieSave}></button>
+                <p className="card__duration">{movie.duration}</p>
             </div>
         </div>
     )
