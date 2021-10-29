2599e34947a58c894062e49939ff228cdca0692c
kt-postnikova <kt-postnikova@yandex.ru>
2021-10-29 12:22:14 +0300

    Добавлено действие по сабмиту поиска фильмов

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,17 +2,19 @@ import React from 'react';
 import searchImage from '../../images/search.svg';
 import findImage from '../../images/find.svg';
 
-function SearchForm(params) {
+function SearchForm({ onSubmit, onChange, query }) {
 
     function handleSubmit(event) {
-        event.preventDefault()
+        event.preventDefault();
+
+        onSubmit();
     }
 
     return (
         <section className="search-form container">
             <form className="search-form__container" onSubmit={handleSubmit} noValidate>
                 <img className="search-form__search-image" src={searchImage} alt="Иконка лупа" />
-                <input className="search-form__input" placeholder="Фильм" type="text" required />
+                <input className="search-form__input" placeholder="Фильм" type="text" value={query} onChange={onChange} required />
                 <button className="search-form__find-image"><img src={findImage} alt="Иконка стрелка" /></button>
             </form>
             <div className="line"></div>
