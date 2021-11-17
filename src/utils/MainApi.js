e287fb928ca19cfa9a06d4193468446c376cdd9e
kt-postnikova <kt-postnikova@yandex.ru>
2021-11-17 13:19:31 +0300

    Добавлены credentials

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,11 +1,12 @@
 const getResponse = response => response.ok ? response.json() : Promise.reject(`Ошибка ${response.status}`)
 
 // const LOCAL_URL = 'http://localhost:3000';
-const PROD_URL = 'https://api.movie-explorer.nomoredomains.club';
+const PROD_URL = 'https://api.movie-explorer.nomoredomains.rocks';
 
 export const register = (email, password, name) => {
     return fetch(`${PROD_URL}/signup`, {
         method: 'POST',
+        credentials: 'include',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
@@ -18,6 +19,7 @@ export const register = (email, password, name) => {
 export const authorize = (email, password) => {
     return fetch(`${PROD_URL}/signin`, {
         method: 'POST',
+        credentials: 'include',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
@@ -30,6 +32,7 @@ export const authorize = (email, password) => {
 export const getContent = () => {
     return fetch(`${PROD_URL}/users/me`, {
         method: 'GET',
+        credentials: 'include',
         headers: {
             "Content-Type": "application/json",
             "Authorization": `${JSON.parse(localStorage.getItem('token'))}`
@@ -41,6 +44,7 @@ export const getContent = () => {
 export const editUserInfo = (userInfo) => {
     return fetch(`${PROD_URL}/users/me`, {
         method: 'PATCH',
+        credentials: 'include',
         headers: {
             'Content-Type': 'application/json',
             "Authorization": `${JSON.parse(localStorage.getItem('token'))}`
@@ -56,6 +60,7 @@ export const editUserInfo = (userInfo) => {
 export const getSavedMovies = () => {
     return fetch(`${PROD_URL}/movies`, {
         method: 'GET',
+        credentials: 'include',
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `${JSON.parse(localStorage.getItem('token'))}`
@@ -67,6 +72,7 @@ export const getSavedMovies = () => {
 export const saveMovie = (movie) => {
     return fetch(`${PROD_URL}/movies`, {
         method: 'POST',
+        credentials: 'include',
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `${JSON.parse(localStorage.getItem('token'))}`
@@ -91,6 +97,7 @@ export const saveMovie = (movie) => {
 export const deleteMovie = (movieId) => {
     return fetch(`${PROD_URL}/movies/${movieId}`, {
         method: 'DELETE',
+        credentials: 'include',
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `${JSON.parse(localStorage.getItem('token'))}`
