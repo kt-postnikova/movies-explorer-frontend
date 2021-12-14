80cf03bc9c01dfd84796810b4772a773f5e67975
kt-postnikova <kt-postnikova@yandex.ru>
2021-12-14 12:03:06 +0300

    Исправлена фильтрация по короткометражкам

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-function FilterCheckbox({ onChecked }) {
+function FilterCheckbox({ onChecked, isChecked }) {
+
     return (
         <div className="checkbox-container container">
             <label className="checkbox__label">
-                <input type="checkbox" onClick={onChecked} />
+                <input type="checkbox" onChange={onChecked} checked={isChecked} />
                 <div className="checkbox__checkmark"></div>
             </label>
             <div className="checkbox__body">Короткометражки</div>
